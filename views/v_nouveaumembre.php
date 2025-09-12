85a46df7869943377677a40d28a725421728833e
Bilal <bilal@berri.com>
2025-09-12 11:55:34 +0200

    first commit

---
+++ views/v_nouveaumembre.php
@@ -0,0 +1,9 @@
+<form method="post" action="index.php?uc=gerer&action=ajouter">
+    <label for="nom">Nom</label><br>
+    <input type="text" name="nom"><br>
+
+    <label for="prenom">Prénom</label><br>
+    <input type="text" name="prenom"><br>
+
+    <button type="submit" class="btn btn-primary">Valider</button>
+</form>
